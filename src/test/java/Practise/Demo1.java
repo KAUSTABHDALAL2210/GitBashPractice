a00e98a3215089d4c64248e4d69b1887a04f4a33
KAUSTABHDALAL2210 <koustabh.dalal@gmail.com>
2023-08-27 21:28:41 +0530

    commited

---
+++ src/test/java/Practise/Demo1.java
@@ -0,0 +1,17 @@
+package Practise;
+
+import org.testng.annotations.Test;
+
+@Test
+public class Demo1 {
+public void demo() {
+	System.out.println("Hi");
+}
+}
+//1st pull
+//2nd push
+//2nd pull
+
+
+//push2
+
